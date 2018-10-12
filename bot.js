a29d2d03f9bfe80f4f132eb9ad2c0d6082c4ca28
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-10-12 19:57:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -365,4 +365,26 @@ if (message.content.startsWith(adminprefix + 'sett')) {
 }
 });
 
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'fire-chat');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',${member})
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , Welcome to the server, ${member})
+        .addField(':id: | user :', "[" + ${member.id} + "]" )
+                .addField(':arrow_right:| انت العضو رقم',${member.guild.memberCount})
+
+                  .addField("Name:",<@ + ${member.id} + >, true)
+
+                                     .addField(' الـسيرفر', ${member.guild.name},true)
+
+     .setFooter("SERVER NAME ")
+        .setTimestamp()
+
+      channel.sendEmbed(em bed);
+    });
+
 client.login(process.env.BOT_TOKEN);
